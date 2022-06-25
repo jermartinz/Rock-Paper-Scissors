07f967cd33e475e304c8dec24f2057f3d7e04fe9
JerMartinz <61643086+jermartinz@users.noreply.github.com>
2022-06-24 21:10:26 -0400

    Add buttons for each choice and round status text

--- main.js
+++ main.js
@@ -3,42 +3,52 @@ const choices = ['paper', 'rock', 'scissors'];
 let humanScore = 0;
 let computerScore = 0;
 let roundStatus = '';
+const resultCtn = document.querySelector('#result-ctn');
+const result = document.createElement('p')
+result.classList.add('result')
+resultCtn.appendChild(result)
 
-function round(){
+
+
+
+function round(playerSelection){
     let computerSelection = choices[Math.floor(Math.random() * choices.length)]; // Randomize choices array and store result
-    let playerSelection = prompt('Select (Rock, Paper or Scissors): ').toLowerCase(); // Ask the user for a choice and store the choice
     // Compare choices store points for each winner and return status of the round
     if(computerSelection === playerSelection){
         roundStatus ='Tie round!';
         humanScore++;
         computerScore++;
-        return `Computer select: ${computerSelection}. Human select: ${playerSelection}. ${roundStatus} Score: Human ${humanScore} - Computer: ${computerScore}`;
+        return result.textContent = `Computer select: ${computerSelection}. Human select: ${playerSelection}. ${roundStatus} Score: Human ${humanScore} - Computer: ${computerScore}`;
+        
+        
     }
     else if((computerSelection === 'rock' && playerSelection === 'scissors') || 
     (computerSelection === 'scissors' && playerSelection === 'paper') || (computerSelection === 'paper' && playerSelection === 'rock')){
         roundStatus ='Computer win this round!';
         computerScore++;
-        return `Computer select: ${computerSelection}. Human select: ${playerSelection}. ${roundStatus} Score: Human ${humanScore} - Computer: ${computerScore}`;
+        return result.textContent = `Computer select: ${computerSelection}. Human select: ${playerSelection}. ${roundStatus} Score: Human ${humanScore} - Computer: ${computerScore}`;
     }else{
         roundStatus = "Human win this round!";
         humanScore++;
-        return `Computer select: ${computerSelection}. Human select: ${playerSelection}. ${roundStatus} Score: Human ${humanScore} - Computer: ${computerScore}`;
+        return result.textContent = `Computer select: ${computerSelection}. Human select: ${playerSelection}. ${roundStatus} Score: Human ${humanScore} - Computer: ${computerScore}`;
+        
     }
+    
 }
 
-function game(){
-    for(let i = 0; i <= 5; i++){ // Call function round() 5 times and print status of each round
-        console.log(round());
-    }
-    // Game end result
-    if(humanScore < computerScore){
-        return 'Computer win!'
-    }
-    else if(humanScore === computerScore){
-        return 'Tie!'
-    }else if(humanScore > computerScore){
-        return 'Human win!'
-    }
-}
-console.log(game());
+const rockBtn = document.querySelector('#rock-btn');
+rockBtn.addEventListener('click', () => {
+    round('rock');
+});
+const paperBtn = document.querySelector('#paper-btn')
+paperBtn.addEventListener('click', () => {
+    round('paper')
+});
+const scissorsBtn = document.querySelector('#scissors-btn')
+scissorsBtn.addEventListener('click', () => {
+    round('scissors')
+});
+
+
+
 
